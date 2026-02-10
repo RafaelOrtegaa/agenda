5e8695c9f05bd0eff5174d19c6e66b9963b0f342
RafaelOrtegaa <rafaelortegacardoso@gmail.com>
2026-02-10 11:09:58 -0300

    refact: alterando o acesso ao caminho do banco de dados para a variável de ambiente (dotenv)

--- models/database.py
+++ models/database.py
@@ -1,10 +1,28 @@
 from sqlite3 import Connection, connect, Cursor
 from types import TracebackType
-from typing import Any, Optional, Self, Type 
+from typing import Any, Optional, Self, Type
+from dotenv import load_dotenv
 import traceback
+import os
+
+load_dotenv() # Procura em um arquivo .env com variáveis
+DB_PATH = os.getenv('DATABASE', './data/tarefas.sqlite3')
+
+def init_db(db_name:str = DB_PATH) -> None:
+    with connect(db_name) as conn:
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS tarefas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            titulo_tarefa TEXT NOT NULL,
+            data_conclusao TEXT  
+        );
+        """)
 
 class Database:
-    def __init__(self, db_name: str) -> None:
+    """
+        Classe que gerencia conexões e operações com um banco de dados SQLite. Utiliza o protocolo de gerenciamento de contexto para garantir que a conexão seja encerrada corretamente. 
+    """
+    def __init__(self, db_name: str = DB_PATH) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
 
@@ -37,21 +55,3 @@ class Database:
             traceback.print_tb(tb)
 
         self.close()
-
-
-
-# try: 
-#     db = Database('./data/tarefas.sqlite3')
-#     db.executar('''
-#     CREATE TABLE IF NOT EXISTS tarefas (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         titulo_tarefa TEXT NOT NULL,
-#         data_conclusao TEXT);        
-#     ''')
-#     db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Estudar Python',
-#     '26-02-02'))
-# except Exception as e:
-#     print(f"Erro ao criar a tabela: {e}")
-# finally:
-#     db.close()
-
