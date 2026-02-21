eae915810beaec0a1831419b2e11189a104cdc5d
RafaelOrtegaa <rafaelortegacardoso@gmail.com>
2026-02-21 15:17:03 -0300

    Adicionando botão de concluído e mudando a estilização do site

--- models/database.py
+++ models/database.py
@@ -5,23 +5,12 @@ from dotenv import load_dotenv
 import traceback
 import os
 
-load_dotenv() # Procura em um arquivo .env com variáveis
+load_dotenv()
+
 DB_PATH = os.getenv('DATABASE', './data/tarefas.sqlite3')
 
-def init_db(db_name:str = DB_PATH) -> None:
-    with connect(db_name) as conn:
-        conn.execute("""
-        CREATE TABLE IF NOT EXISTS tarefas(
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            titulo_tarefa TEXT NOT NULL,
-            data_conclusao TEXT  
-        );
-        """)
 
 class Database:
-    """
-        Classe que gerencia conexões e operações com um banco de dados SQLite. Utiliza o protocolo de gerenciamento de contexto para garantir que a conexão seja encerrada corretamente. 
-    """
     def __init__(self, db_name: str = DB_PATH) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
@@ -30,28 +19,30 @@ class Database:
         self.cursor.execute(query, params)
         self.connection.commit()
         return self.cursor
-
+    
+    def buscar_um(self, query: str, params: tuple = ()) -> Any:
+        self.cursor.execute(query, params)
+        return self.cursor.fetchone()
+    
     def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
-
+    
     def close(self) -> None:
         self.connection.close()
 
-    # Métodos para o gerenciamento de contexto
-
-    # Método de entrada do contexto
     def __enter__(self) -> Self:
         return self
     
-    # Método de saída de contexto
-    def __exit__(self, exc_type: Optional[Type[BaseException]], exc_value: Optional[BaseException], tb: Optional[TracebackType]) -> None:
-
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]], 
+                 exc_value: Optional[BaseException], 
+                 tb: Optional[TracebackType]) -> None:
         if exc_type is not None:
-            print('Exceção caprturada no contexto:')
-            print(f'Tipo: {exc_type.__name__}')
+            print("Exceção capturada no contexto:")
+            print(f'tipo: {exc_type.__name__}')
             print(f'Mensagem: {exc_value}')
-            print('Traceback completo:')
+            print('Traceback Completo: ')
             traceback.print_tb(tb)
 
         self.close()
